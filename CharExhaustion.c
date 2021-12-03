e6ddb670c23e6fd89167605d2fad5c7088623885
limithink <limithink@live.com>
2021-12-03 11:39:58 +0800

    Snapshot_20211203

--- CharExhaustion.c
+++ CharExhaustion.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 #define FIELDNAME "\t+0\t+1\t+2\t+3\t+4\t+5\t+6\t+7\t+8\t+9\t+A\t+B\t+C\t+D\t+E\t+F"
 #define TABSTR "\t"
@@ -33,9 +32,9 @@ char* Byte2Hex(unsigned char* pByte, int isLE, int isLower)
 		else {
 			//A-F or a-f
 			if (isLower)
-				Hex[ctr] += 0x61;//Lowercase
+				Hex[ctr] += 0x57;//Lowercase
 			else
-				Hex[ctr] += 0x41;//Uppercase
+				Hex[ctr] += 0x37;//Uppercase
 		}
 	}
 	return Hex;
@@ -62,17 +61,17 @@ void Int2ByteArr(unsigned char* byteArr, int byteNum, size_t intVal, int isLE)
 	return;
 }
 
-void memDstCurCpy(void* cursor, void* src, int cpySize)
+void memDstCurCpy(void** pCursor, void* src, int cpySize)
 {
-	memcpy(cursor, src, cpySize);
-	(char*)cursor += cpySize;
+	memcpy(*pCursor, src, cpySize);
+	*((char**)pCursor) += cpySize;
 }
 
-void ExhaustProc(int byteNum, FILE* fp, char* pLFstr, int lenStrLF, int isLE)
+void ExhaustProc(size_t byteNum, FILE* fp, char* pLFstr, int lenStrLF, int isLE)
 //isLE controls Bytes Order of Single Charactor to be Little-Endian or Big-Endian
 {
 	//FieldRow of char Block Constructing
-	int lenFieldRow = byteNum * 2 + g_lenFieldName;
+	size_t lenFieldRow = byteNum * 2 + g_lenFieldName;
 	char* strFieldRow = (char*)calloc(lenFieldRow, sizeof(char));
 	for (int ctr = 0; ctr < byteNum; ctr++)
 	{
@@ -80,66 +79,95 @@ void ExhaustProc(int byteNum, FILE* fp, char* pLFstr, int lenStrLF, int isLE)
 		*(strFieldRow + ctr + byteNum) = 0x20;
 	}
 	memcpy(strFieldRow + byteNum * 2, g_fieldName, g_lenFieldName);
-	//Allocating Buffer of char Block & byte Array
-	int lenCharBlk = lenFieldRow + lenStrLF + \
+	//Allocating Buffer of char Block 
+	size_t lenCharBlk = lenFieldRow + lenStrLF + \
 		(byteNum * 2 + (g_lenStrTab + byteNum) * 16 + lenStrLF) * 16 + \
 		lenStrLF;
-	int byteNum_ = byteNum - 1;
-	int byteNum_D = byteNum_ * 2;
-	unsigned char* byteArr = (unsigned char*)calloc(byteNum_, sizeof(unsigned char));
-	char* hexArr = (char*)calloc(byteNum_D, sizeof(char));
 	char* blk = (char*)calloc(lenCharBlk, sizeof(char));
 	char* base = blk;
-	//Write Field Row to File and LineFeed of End
-	memDstCurCpy(base, strFieldRow, lenFieldRow);
-	memcpy(blk + lenCharBlk - lenStrLF, pLFstr, lenStrLF);
-	//Iteration of a char Block
-	for (size_t counter = 0; counter < 0x1 << (4 * (byteNum_)); counter++)
-	{
-		char* cursor = base;
-		//Integer Counter => Byte Array
-		Int2ByteArr(byteArr, byteNum_, counter, isLE);
-		//Byte Array => Hex String
-		if (isLE) {
-			//Little-Endian Bytes Order of Single Charactor
-			for (int ctr = byteNum_ - 1; ctr >= 0; ctr--)
-				memcpy(hexArr + ctr * 2, Byte2Hex(byteArr + ctr, NULL, NULL), 2);
+	//Write Field Row to File and LineFeed of End to char Block Buffer
+	memDstCurCpy(&base, strFieldRow, lenFieldRow);
+	memDstCurCpy(&base, pLFstr, lenStrLF);
+	free(strFieldRow);//free Mem used for Constructing Field Row
+	if (byteNum > 1)
+	{//Exhausting Characters of 2 Bytes or more 
+		//Allocating Buffer of Byte Array and Corresponding Hex Array
+		size_t byteNum_ = byteNum - 1;
+		size_t byteNum_D = byteNum_ * 2;
+		unsigned char* byteArr = (unsigned char*)calloc(byteNum_, sizeof(unsigned char));
+		char* hexArr = (char*)calloc(byteNum_D, sizeof(char));
+		//Iteration of a char Block
+		for (size_t counter = 0; counter < 0x1 << (byteNum_ * 8); counter++)
+		{
+			char* cursor = base;
+			//Integer Counter => Byte Array
+			Int2ByteArr(byteArr, byteNum_, counter, isLE);
+			//Byte Array => Hex String
+			if (isLE) {
+				//Little-Endian Bytes Order of Single Charactor
+				for (int ctr = byteNum_ - 1; ctr >= 0; ctr--)
+					memcpy(hexArr + ctr * 2, Byte2Hex(byteArr + ctr, NULL, NULL), 2);
+			}
+			else {
+				//Big-Endian Bytes Order of Single Charactor
+				for (int ctr = 0; ctr < byteNum_; ctr++)
+					memcpy(hexArr + ctr * 2, Byte2Hex(byteArr + ctr, NULL, NULL), 2);
+			}
+			//Row Iteration
+			for (int row = 0x0; row < 0x100; row += 0x10)
+			{
+				//Write Hex String in each Row Head
+				memDstCurCpy(&cursor, hexArr, byteNum_D);
+				memDstCurCpy(&cursor, Byte2Hex(&row, NULL, NULL), 2);
+				//Col Iteration
+				for (int col = 0x0; col < 0x10; col++)
+				{
+					memDstCurCpy(&cursor, TABSTR, g_lenStrTab);
+					unsigned char endian = (unsigned char)(row + col);
+					if (isLE) {
+						//Little-Endian Bytes Order of Single Charactor
+						memDstCurCpy(&cursor, &endian, 1);
+						memDstCurCpy(&cursor, byteArr, byteNum_);
+					}
+					else {
+						//Big-Endian Bytes Order of Single Charactor
+						memDstCurCpy(&cursor, byteArr, byteNum_);
+						memDstCurCpy(&cursor, &endian, 1);
+					}
+				}
+				//Write LineFeed to File of Each Row End
+				memDstCurCpy(&cursor, pLFstr, lenStrLF);
+			}
+			//Write LineFeed to File of Each Char Block End
+			memDstCurCpy(&cursor, pLFstr, lenStrLF);
+			//Buffer => File
+			fwrite(blk, sizeof(char), lenCharBlk, fp);
 		}
-		else {
-			//Big-Endian Bytes Order of Single Charactor
-			for (int ctr = 0; ctr < byteNum_; ctr++)
-				memcpy(hexArr + ctr * 2, Byte2Hex(byteArr + ctr, NULL, NULL), 2);
-		}
+		free(byteArr);
+		free(hexArr);
+	}
+	else
+	{//Exhausting Characters of Only a Byte
 		//Row Iteration
 		for (int row = 0x0; row < 0x100; row += 0x10)
 		{
 			//Write Hex String in each Row Head
-			memDstCurCpy(cursor, hexArr, byteNum_D);
-			memDstCurCpy(cursor, Byte2Hex(&row, NULL, NULL), 2);
+			memDstCurCpy(&base, Byte2Hex(&row, NULL, NULL), 2);
 			//Col Iteration
 			for (int col = 0x0; col < 0x10; col++)
 			{
-				memDstCurCpy(cursor, TABSTR, g_lenStrTab);
-				unsigned char endian = row + col;
-				if (isLE) {
-					//Little-Endian Bytes Order of Single Charactor
-					memDstCurCpy(cursor, &endian, 1);
-					memDstCurCpy(cursor, byteArr, byteNum_);
-				}
-				else {
-					//Big-Endian Bytes Order of Single Charactor
-					memDstCurCpy(cursor, byteArr, byteNum_);
-					memDstCurCpy(cursor, &endian, 1);
-				}
+				memDstCurCpy(&base, TABSTR, g_lenStrTab);
+				unsigned char endian = (unsigned char)(row + col);
+				memDstCurCpy(&base, &endian, 1);
 			}
+			//Write LineFeed to File of Each Row End
+			memDstCurCpy(&base, pLFstr, lenStrLF);
 		}
+		//Write LineFeed to File of Each Char Block End
+		memDstCurCpy(&base, pLFstr, lenStrLF);
 		//Buffer => File
 		fwrite(blk, sizeof(char), lenCharBlk, fp);
 	}
-	//Mem Discharge
-	free(strFieldRow);
-	free(byteArr);
-	free(hexArr);
 	free(blk);
 }
 
@@ -163,11 +191,11 @@ int main(int argc, char** argv)
 		printf("\nCreating File Failed!!!");
 		return 0;
 	}
-	for (int ctr = 0; ctr < byteNum; ctr++)
+	for (int ctr = 1; ctr <= byteNum; ctr++)
 	{
-		fprintf(fp, "CharExhaustion of %d Byte:\n\n", ctr);
-		ExhaustProc(ctr + 1, fp, "\n\r", sizeof("\n\r") - 1, NULL);
-		fprintf(fp, "\n\n\n");
+		fprintf(fp, "CharExhaustion of %d Byte:\r\n\r\n", ctr);
+		ExhaustProc(ctr, fp, "\r\n", sizeof("\r\n") - 1, NULL);
+		fprintf(fp, "\r\n");
 	}
 	fclose(fp);
 	printf("Complete");
